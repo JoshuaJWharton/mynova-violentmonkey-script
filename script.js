a551a71783c8847d9c5ee7e8af36a543d454bf1b
JoshuaJWharton <157727223+JoshuaJWharton@users.noreply.github.com>
2024-01-26 11:56:21 -0500

    Fixed some carelessness

--- script.js
+++ script.js
@@ -105,10 +105,8 @@ function setMyStyle() {
   // Get the headers into some nice boxes
   $('h2').each(function(){$(this).attr('style','background-color: '+NovaBlue+';color: rgb(255,255,255);padding:6px;border:1px solid #ffffff');});
 
-  SidebarExpanded=SidebarShadowString+SidebarLeftExpandedString+SidebarColorWhiteString;
-    SidebarCollapsed=SidebarShadowString+SidebarLeftCollapsedString+SidebarColorBlueString;  
   SidebarShadowString='box-shadow:' + BoxShadowDepth + 'px ' + BoxShadowDepth + 'px;';
-      $('sd-sidebar').attr('style',Expanded?SidebarExpanded:SidebarCollapsed);
+      $('sd-sidebar').attr('style',SidebarShadowString);
   
     /* Sidebar is NOT that annoying. Let's keep it for now
   // Sidebar that can move in or out, and moves the rest of the screen's content with it
